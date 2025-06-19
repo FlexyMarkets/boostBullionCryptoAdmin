58a670507b1fc0ab6dacc23f47ae5460a3e54c06
Aditya Shaw <adityashaw9066@gmail.com>
2025-06-19 15:53:19 +0530

    ticket list table

--- src/userPanel/pages/helpDesk/myTickets/MyTickets.jsx
+++ src/userPanel/pages/helpDesk/myTickets/MyTickets.jsx
@@ -1,14 +1,16 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Stack, Typography } from '@mui/material';
 import TicketsTable from './ticketsTable/TicketsTable';
 
 
 function MyTickets() {
 
   return (
-    <Container sx={{ mt: "100px" }}>
+    // <Container sx={{ mt: "100px" }}>
+    <Stack sx={{ mt: "100px", mx: "5rem" }}>
       <Typography variant='h5' fontWeight={"700"} fontSize={"1.8rem"}>All Tickets</Typography>
       <TicketsTable />
-    </Container>
+    </Stack>
+    // {/* </Container> */ }
   )
 }
 
